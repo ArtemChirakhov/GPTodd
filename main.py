a0e3aa470521323c62e685235a83f2a97a48c53e
Vova <vovadelis@gmail.com>
2023-04-12 18:09:15 +0300

    Более работающая основа бота

--- main.py
+++ main.py
@@ -9,26 +9,29 @@ intents.message_content = True
 class Cog_Todd(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        print("Cog is ready")
 
     @commands.command(name="chat")
     async def chat(self, message):
-        if message.author == bot.user:
-            return
         await message.channel.send("Ready to chat")
-        self.chat = True
-
-class Client_Todd(discord.Client):
-    async def on_ready(self):
-        print("Bot is ready")
-
-    async def on_message(self, message):
-        if message.author == self.user:
-            return
-        message.channel.send("Hello")
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 TOKEN = open("TOKEN.txt").readline()
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    for guild in bot.guilds:
+        print(
+            f'{bot.user} подключились к чату:\n'
+            f'{guild.name}(id: {guild.id})')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    await message.channel.send("Hello")
+
+
 
 async def main():
     await bot.add_cog(Cog_Todd(bot))
